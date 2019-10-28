19ba10c5a955c061fcfeaff9e928e13c124c7cc3
Alexandra Santos <alexandraps17@gmail.com>
2019-10-28 17:53:17 +0000

    fetch done

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'; 
 import './Home.css';
 
-const Home = () => (
-    <h1> Home </h1>
-); 
+const Home = ({randomUser, newUser}) => {
+    console.log("user banana" , randomUser.name)
+    return (
+        <div>
+            {
+                randomUser.name!==undefined && 
+                <div>
+                    <img src={randomUser.picture.large} alt="Tender user"/>
+                    <h1> {randomUser.name.first} {randomUser.name.last} </h1>
+                    <h2> {randomUser.dob.age} years old </h2>
+                </div>
+            }
+         
+            <button onClick ={newUser}> Nobody loves you</button>
+         
+            <button> Marry me </button>
+            <button> More info </button> 
+        </div>
+    )
+}
 
 export default Home; 
