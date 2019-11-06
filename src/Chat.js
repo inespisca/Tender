788a3195e5c027ea2369c7184269f236596fa4f3
Alexandra Santos <alexandraps17@gmail.com>
2019-11-06 14:36:48 +0000

    added some bootstrap

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'; 
 import './Chat.css';
 import ChatMessages from "./ChatMessages"; 
+import {Container, Row, Col} from "react-bootstrap"
 
 class Chat extends Component {
     constructor(props) {
@@ -19,36 +20,43 @@ class Chat extends Component {
     
     sendMessageNow = e => {
         e.preventDefault();
-        this.setState((state) => {
-            return {
-                ...state, 
-                sentMessages: [...state.sentMessages, state.userMessage],
-                timeNow : new Date(),
-                userMessage: "",
-            }
-        } )
+        if(this.state.userMessage){
+          this.setState((state) => {
+              return {
+                  ...state, 
+                  sentMessages: [...state.sentMessages, state.userMessage],
+                  timeNow : new Date(),
+                  userMessage: "",
+              }
+            } )
+        }
     }
 
   render () {
     return (
-        <>
-        <div className="chat-sentMessages">
+        <Container>
+          <Row >
             <ChatMessages sentMessages={this.state.sentMessages} timeNow={this.state.timeNow}/> 
-        </div>
-          <form className= "chat-chatForm">
-              <input 
-                type="text" 
-                className="chat-textInput" 
-                placeholder="Go ahead, send them a message. Don't be shy!" 
-                value={this.state.userMessage} 
-                onChange={this.showCurrentlyTyping}/>
-              <input 
-                type="submit" 
-                className="chat-sendButton" 
-                value="Send" 
-                onClick={this.sendMessageNow}/>
-          </form>
-        </>
+          </Row>
+           <Row>
+              <form 
+                class="fixed-bottom" 
+                className="chat-chatForm"
+              >
+                  <input 
+                    type="text" 
+                    className="chat-textInput" 
+                    // placeholder="Go ahead, send them a message. Don't be shy!" 
+                    value={this.state.userMessage} 
+                    onChange={this.showCurrentlyTyping}/>
+                  <input 
+                    type="submit" 
+                    className="chat-sendButton" 
+                    value="Send" 
+                    onClick={this.sendMessageNow}/>
+              </form>
+           </Row>
+        </Container>
 
     );
   }
--- src/ChatMessages.js
+++ src/ChatMessages.js
@@ -1,20 +1,24 @@
 import React from "react"
 import "./ChatMessages.css"
-import { Container } from "react-bootstrap";
-// import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from "react-bootstrap";
 
 const ChatMessage = ({sentMessages, timeNow}) => {
 
 return(
 //still need to add time 
     <Container>
-        <div>
-            {sentMessages.map ( message => (
-                <div className="chatMessages-newMessage">
-                    <p className="chatMessages-text"> {message} </p>
-                </div>
-            ))}
-        </div>
+        <Row>
+        <Col  md={{ span: 4, offset: 8 }} sm={{ span: 5, offset: 7 }} xs={{ span: 8, offset: 4 }} >
+                {sentMessages.map ( message => (
+                    <div className="chatMessages-eachMessage">
+                        <div className="chatMessages-newMessage">
+                            <p> {message} </p>
+                        </div>
+                        <div className="chatMessages-spaceBetweenMessages"></div>
+                    </div>
+                ))}
+            </Col>
+        </Row>
     </Container>
 
 )
