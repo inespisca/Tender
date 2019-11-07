1ccaafc1856890603284a4c7787c627165e6a00f
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-07 17:21:35 +0000

    css and flex instead of bootstrap

--- src/Chat.js
+++ src/Chat.js
@@ -1,62 +1,62 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
 import './Chat.css';
-import ChatMessages from "./ChatMessages"; 
-import {Row} from "react-bootstrap";
+import ChatMessages from "./ChatMessages";
+import { Row } from "react-bootstrap";
 import Messages from "./Messages";
 
 class Chat extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = { 
-        userMessage: "", 
-        sentMessages: [],
-        timeNow: "still not changed" , 
-      };
-    }
+  constructor(props) {
+    super(props);
 
-    showCurrentlyTyping = e => {
-        this.setState({userMessage: e.target.value});
-    } 
-    
-    sendMessageNow = e => {
-        e.preventDefault();
-        if(this.state.userMessage){
-          this.setState((state) => {
-              return {
-                  ...state, 
-                  sentMessages: [...state.sentMessages, state.userMessage],
-                  timeNow : new Date(),
-                  userMessage: "",
-              }
-            } )
+    this.state = {
+      userMessage: "",
+      sentMessages: [],
+      timeNow: "still not changed",
+    };
+  }
+
+  showCurrentlyTyping = e => {
+    this.setState({ userMessage: e.target.value });
+  }
+
+  sendMessageNow = e => {
+    e.preventDefault();
+    if (this.state.userMessage) {
+      this.setState((state) => {
+        return {
+          ...state,
+          sentMessages: [...state.sentMessages, state.userMessage],
+          timeNow: new Date(),
+          userMessage: "",
         }
+      })
     }
+  }
 
-  render () {
+  render() {
     return (
       <div className="chat">
         <div className="chat-contacts">
-          <Messages randomUsers={this.props.randomUsers}/>
+          <Messages randomUsers={this.props.randomUsers} />
         </div>
 
         <div className="chat-area">
           <div className="chat-spaceForMessages">
             <div className="chat-spaceForMessagesScroll">
-              <ChatMessages sentMessages={this.state.sentMessages} timeNow={this.state.timeNow}/> 
+              <ChatMessages sentMessages={this.state.sentMessages} timeNow={this.state.timeNow} />
             </div>
             <form className="chat-chatForm">
-              <input 
-                type="text" 
-                className="chat-textInput" 
+              <input
+                type="text"
+                className="chat-textInput"
                 // placeholder="Go ahead, send them a message. Don't be shy!" 
-                value={this.state.userMessage} 
-                onChange={this.showCurrentlyTyping}/>
-              <input 
-                type="submit" 
-                className="chat-sendButton" 
-                value="Send" 
-                onClick={this.sendMessageNow}/>
+                value={this.state.userMessage}
+                onChange={this.showCurrentlyTyping} />
+              <input
+                type="submit"
+                className="chat-sendButton"
+                value="Send"
+                onClick={this.sendMessageNow} />
             </form>
           </div>
         </div>
--- src/Messages.js
+++ src/Messages.js
@@ -1,9 +1,6 @@
 
 import './Messages.css';
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Message from './Message';
 import React, { Component } from 'react';
 
 class Messages extends Component {
@@ -13,31 +10,7 @@ class Messages extends Component {
             <div>
                 {
                     randomUsers.map(user => {
-                        return (
-                            <Container>
-                                <div key={user.name.first}>
-                                    <Row>
-                                        <Col lg={1} md={6} >
-                                            <Link to="/chat" className="link">
-                                                <img src={user.picture.thumbnail} alt="Tender user" className="avatar" />
-                                            </Link>
-                                        </Col>
-                                        <Col lg={2} md={2}>
-                                            <Link to="/chat" className="link">
-                                                {user.name.first} {user.name.last}
-                                            </Link>
-                                        </Col>
-                                        <Col lg={8} md={5} className="text">
-                                            <Link to="/chat" key={user.name} className="text" className="link">
-                                                {user.message ? user.message : "In my local directory a file was created titled .sofia/ and there is a file in there called phoneNum.txt which is corrupted. I use that file to execute a program called date.py. Do you know how to correct that file? "}
-                                            </Link>
-                                        </Col>
-                                        <Col lg={1} md={2} className="date"> {user.date ? user.date : "29 oct."}
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </Container>
-                        )
+                        return <Message user={user} key={user.name.first} />
                     })
                 }
             </div>
