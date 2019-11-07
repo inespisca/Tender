a9817d277c2dac010c4a71d661096c470d874131
Alexandra Santos <alexandraps17@gmail.com>
2019-11-07 16:16:41 +0000

    send them a message+logo

--- src/Chat.js
+++ src/Chat.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'; 
 import './Chat.css';
-import ChatMessages from "./ChatMessages"; 
-// import {Row} from "react-bootstrap";
 import Messages from "./Messages";
+import ChatMessagesDisplay from "./ChatMessagesDisplay"
 
 class Chat extends Component {
     constructor(props) {
@@ -42,15 +41,14 @@ class Chat extends Component {
         </div>
 
         <div className="chat-spaceForMessages">
-          {/* <div className=""> */}
             <div className="chat-spaceForMessagesScroll">
-              <ChatMessages sentMessages={this.state.sentMessages} timeNow={this.state.timeNow}/> 
+              <ChatMessagesDisplay sentMessages={this.state.sentMessages} timeNow={this.state.timeNow} />
             </div>
+
             <form className="chat-chatForm">
               <input 
                 type="text" 
                 className="chat-textInput" 
-                placeholder="Go ahead, send them a message. Don't be shy!" 
                 value={this.state.userMessage} 
                 onChange={this.showCurrentlyTyping}/>
               <input 
@@ -60,7 +58,6 @@ class Chat extends Component {
                 onClick={this.sendMessageNow}/>
             </form>
           </div>
-        {/* </div> */}
       </div>
     );
   }
