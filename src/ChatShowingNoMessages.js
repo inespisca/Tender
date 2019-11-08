24ff1b0a20c50bba1b8ce025d5bbf23d842f8894
Alexandra Santos <alexandraps17@gmail.com>
2019-11-08 16:34:08 +0000

    sorry

---
+++ src/ChatShowingNoMessages.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ChatShowingNoMessages.css"
+
+const ChatShowingNoMessages = () => {
+
+return(
+    <div className="chat-noMessages">
+        <img
+        className="tenderLogo" 
+        src="https://i.imgur.com/LN19krz.png" alt="Tender Logo"></img>
+        <h1> Go ahead, send them a message. <br/> Don't be shy! </h1>
+
+    </div>
+)
+}
+export default ChatShowingNoMessages; 
