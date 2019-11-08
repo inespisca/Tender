63ac19dd74adeb2764f3281aa7a9492014af2195
Alexandra Santos <alexandraps17@gmail.com>
2019-11-08 14:36:22 +0000

    Revert "fix conflicts revert"
    This reverts commit 51dd4391def995501496c686988d07f8b16fced4, reversing
    changes made to e18a66198805187b33139ab24c71dd84b3ec58ce.

---
+++ src/Settings.js
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import './Settings.css';
+import { Radio } from 'semantic-ui-react';
+
+const Settings = ({ settings, onChange }) => {
+    return (
+        <div>
+            <h1> Settings </h1>
+            <div>
+                Smoker: <span><Radio toggle checked={settings.smoker} onChange={() => onChange('smoker')}/></span>
+            </div>
+            <div>
+                Vegetarian: <span><Radio toggle checked={settings.vegetarian} onChange={() => onChange('vegetarian')}/></span>
+            </div>
+            <div>
+                Single: <span><Radio toggle checked={settings.single} onChange={() => onChange('single')}/></span>
+            </div>
+        </div> 
+    )
+}
+//These settings are connected to the state in the App through the string at the end of each one. Ex: Smoker is connected to "smoker: false," in the App
+//thanks to the 'smoker' in "onChange('smoker')}". This means that if you switch the string text to 'Smoker' (with capital "S"), the radio toggle will
+//stop working.
+
+export default Settings; 
