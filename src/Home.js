1628158320a3a8638154bae719f8a0e927019cc9
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-12 15:52:37 +0000

    more css

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,13 @@ const Home = ({ randomUser, newUser, chuckNorrisQuote }) => {
                 {
                     randomUser.name !== undefined &&
                     <div className="swipe-area">
-                        <button onClick={newUser}> NOBODY LOVES YOU</button>
+                        <button onClick={newUser}>NOBODY LOVES YOU</button>
                         <div className="wrapper">
                             <img className="mainImage" src={randomUser.picture.large} alt="Tender user" />
                             <h1> {randomUser.name.first} {randomUser.name.last} </h1>
                             <h2> {randomUser.dob.age} years old </h2>
                         </div>
-                        <button> MARRY ME</button>
+                        <button>MARRY ME</button>
                     </div>
 
                 }
