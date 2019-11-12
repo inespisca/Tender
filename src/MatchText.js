0699044fb3d710dfc5397c5d3f3f7de3b6048f2f
Alexandra Santos <alexandraps17@gmail.com>
2019-11-12 17:51:37 +0000

    yes path completed

--- src/MatchText.js
+++ src/MatchText.js
@@ -1,32 +1,51 @@
 import React from "react";
 import "./MatchText.css";
-import { Redirect } from 'react-router-dom';
-
+import { withRouter, useHistory } from "react-router-dom";
 
 const MatchText = ({isMatch, handleNewUser}) => {
 
+    let history = useHistory ()
+
+    const handleRedirectToChat = () => {
+        history.push("/chat");
+        handleNewUser()
+    }
+
     if(isMatch){
         return (
-            <div className ="noMatch">
-            <h1> Sorry, they don't like you back. Better luck next time. </h1>
-            <button  
-                className="matchText-newUser"
-                onClick={handleNewUser}> Keep searching </button>
+            <div className ="isMatch">
+                <div className="noMatch">
+                    <h1> Sorry, they don't like you back. <br/> Better luck next time. </h1>
+                    <button  
+                        className="matchText-newUser"
+                        onClick={handleNewUser}
+                    > Keep searching </button>
+                </div>
             </div>
         )
     } else{
         return (
-            <div className ="yesMatch">
-            <h1> They like you back! </h1>
-            <button> Send them a message </button>
-            <button 
-                className="matchText-newUser"
-                onClick={handleNewUser}
-            > Keep searching  </button>
+            <div className ="isMatch">
+                <div className ="yesMatch">                    
+                    <h1> They like you back </h1>
+                    <img
+                        className="matchText-tenderLogo" 
+                        src="https://i.imgur.com/LN19krz.png" alt="Tender Logo">
+                    </img>
+                </div>
+                <div className="matchText-yesBtns">
+                    <button className="matchText-goToChatBtn" 
+                        onClick={handleRedirectToChat}
+                    > Send them a message </button>
+                    <button 
+                        className="matchText-newUser"
+                        onClick={handleNewUser}
+                    > Keep searching  </button>
+                </div> 
             </div>
         )
     }  
 
 }
 
-export default MatchText; 
+export default withRouter (MatchText); 
