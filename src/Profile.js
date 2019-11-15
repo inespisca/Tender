9fa9971a56167e49a3ac2fe070fcbf1eab9c1c24
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-15 14:20:12 +0000

    some CSS but much more to come

--- src/Profile.js
+++ src/Profile.js
@@ -13,28 +13,43 @@ const Profile = ({ user, settings }) => {
     }
 
     return (
-        <>
-            <div className="description-container">
-                <div classeName="profile-pict-container">
-                    <img src={user.picture.large} alt="Tender user" className="profile-pict" />
-                </div>
-
-                <Description user={user} settings={settings} />
-                <div>
-                    {user.contact ? user.contact.LinkedIn : ""}
-                </div>
-                <div>
-                    {user.contact ? user.contact.GitHub : ""}
-                </div>
-                <button className="matchText-goToChatBtn"
-                    onClick={handleRedirectToChat}
-                > Send a message </button>
+        <div classeName="profile-container">
+            <div className="profile-pict">
+                <img src={user.picture.large} alt="Tender user" className="pict" />
             </div>
+            <div className="profile-description">
+                <div className="profile-social-links">
+                    {(user.name.first === "Angélina" || user.name.first === "Alexandra" || user.name.first === "Inês" || user.name.first === "Elena") &&
+                        <>
+                            <div className="social-links">
+                                <div>
+                                    <a href={user.contact.LinkedIn} className="social-link">
+                                        <i class="fab fa-linkedin-in "></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={user.contact.GitHub} className="social-link">
+                                        <i class="fab fa-github"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </>
+                    }
+                </div>
 
-
-        </>
-
+                <div className="profile-description">
+                    <Description user={user} settings={settings} className="profile-description" />
+                </div>
+                <div className="profile-goToChatBtn">
+                    <button
+                        onClick={handleRedirectToChat}
+                    > Send a message </button>
+                </div>
+            </div>
+        </div>
     )
+
+
 }
 
 
