2efe45a9b0757947a2bfd16806f67bec83c225e9
Inês Oliveira <inesfpoliveira@gmail.com>
2019-11-19 13:08:32 +0000

    change of the settings colours background and attempts of responsiveness that I should keep working in later

--- src/Settings.js
+++ src/Settings.js
@@ -6,10 +6,12 @@ import { Radio } from 'semantic-ui-react';
 const Settings = ({ settings, onChange }) => {
     return (
         <div className="settings-container">
-            <h2> I'm looking for... </h2>
-            <Preferences settings={settings} onChange={onChange}/>
-            <div className="border">
+            <div className="looking-for">
+                <p classname="settingsTitle">
+                    I'm looking for...
+                </p>
             </div>
+            <Preferences settings={settings} onChange={onChange}/>
             <div className="setting">
                 <p className="settingsItem">
                     Smoker:
@@ -18,8 +20,6 @@ const Settings = ({ settings, onChange }) => {
                     <Radio toggle checked={settings.smoker} onChange={() => onChange('smoker')}/>
                 </span>
             </div>
-            <div className="border">
-            </div>
             <div className="setting">
                 <p className="settingsItem">
                     Vegetarian:
@@ -27,8 +27,6 @@ const Settings = ({ settings, onChange }) => {
                 <span>
                     <Radio toggle checked={settings.vegetarian} onChange={() => onChange('vegetarian')}/>
                 </span>
-            </div>
-            <div className="border">
             </div>
             <div className="last-setting">
                 <p className="settingsItem">
