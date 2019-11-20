528902843ac0c05225224180a12c566750ff17cd
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-20 09:43:17 +0000

    Updates

--- precache-manifest.1903fa729db45b856c51321dfdfbae10.js
+++ precache-manifest.1903fa729db45b856c51321dfdfbae10.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "002f3373264364a65707f85f979f9e6e",
+    "revision": "c3429906db201850296ffaad38c054c0",
     "url": "/Tender/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Tender/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "22c35b1939a03ea496a0",
-    "url": "/Tender/static/css/main.4cf630d5.chunk.css"
+    "revision": "a5ebd59cc90e76883f05",
+    "url": "/Tender/static/css/main.46bb42d4.chunk.css"
   },
   {
     "revision": "344e6590fb72f17ee53f",
     "url": "/Tender/static/js/2.8b7e211b.chunk.js"
   },
   {
-    "revision": "22c35b1939a03ea496a0",
-    "url": "/Tender/static/js/main.695ccb9f.chunk.js"
+    "revision": "a5ebd59cc90e76883f05",
+    "url": "/Tender/static/js/main.6cd5fbef.chunk.js"
   },
   {
     "revision": "7883dbc8b14f3a784147",
