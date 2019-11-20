760c8c3091eb05cc5b7f7f86a108762b705701ce
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-20 11:57:33 +0000

    logo for mobile added

--- build/precache-manifest.003454bc5f26347cbe6de2169e14cbc5.js
+++ build/precache-manifest.003454bc5f26347cbe6de2169e14cbc5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "002f3373264364a65707f85f979f9e6e",
+    "revision": "d431cad9f5ec512b5ba728c78c62db57",
     "url": "/Tender/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Tender/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "22c35b1939a03ea496a0",
-    "url": "/Tender/static/css/main.4cf630d5.chunk.css"
+    "revision": "2cbf99ed42598b6b58c0",
+    "url": "/Tender/static/css/main.fa5aae90.chunk.css"
   },
   {
     "revision": "344e6590fb72f17ee53f",
     "url": "/Tender/static/js/2.8b7e211b.chunk.js"
   },
   {
-    "revision": "22c35b1939a03ea496a0",
-    "url": "/Tender/static/js/main.695ccb9f.chunk.js"
+    "revision": "2cbf99ed42598b6b58c0",
+    "url": "/Tender/static/js/main.6cd5fbef.chunk.js"
   },
   {
     "revision": "7883dbc8b14f3a784147",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Tender/precache-manifest.3ada43832df5936d4087a6840057d6c9.js"
+  "/Tender/precache-manifest.003454bc5f26347cbe6de2169e14cbc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tender/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
